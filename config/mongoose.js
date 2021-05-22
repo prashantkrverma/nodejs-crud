27f738d303d36175522ef4189dd8df3b4ef640be
Prashant Kumar Verma <prashantkrverma51@gmail.com>
2021-05-23 03:45:14 +0530

    env updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,7 +7,7 @@ mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose
-  .connect(CONFIG[NODE_ENV].mongourl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(CONFIG.development.mongourl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("DB Connected"))
   .catch(err => console.log(err));
 
