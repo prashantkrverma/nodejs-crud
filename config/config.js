14b4fe5b80111186238d17f25cae2bc6408670e9
Prashant Kumar Verma <prashantkrverma51@gmail.com>
2021-05-23 03:20:55 +0530

    npm script added

--- config/config.js
+++ config/config.js
@@ -3,6 +3,16 @@ module.exports = {
       port: process.env.PORT || 3000,
       mongourl: `${process.env.DB_HOST}+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f7suv.${process.env.DB_HOST}.net/${process.env.DB}?retryWrites=true&w=majority`,
       db: process.env.DB
+    },
+    staging: {
+      port: process.env.PORT || 3000,
+      mongourl: `${process.env.DB_HOST}+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f7suv.${process.env.DB_HOST}.net/${process.env.DB}?retryWrites=true&w=majority`,
+      db: process.env.DB
+    },
+    production: {
+      port: process.env.PORT || 3000,
+      mongourl: `${process.env.DB_HOST}+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f7suv.${process.env.DB_HOST}.net/${process.env.DB}?retryWrites=true&w=majority`,
+      db: process.env.DB
     }
   };
 
